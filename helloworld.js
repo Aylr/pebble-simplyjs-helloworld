fca0808f3898569096e4770e5287cb89602a3fb6
aylr <taylormiller@gmail.com>
2015-07-06 07:08:59 -0600

    3

--- helloworld.js
+++ helloworld.js
@@ -1,9 +1,10 @@
 simply.title('Sprinklers!');
+simply.body("0 Front\n
+0 Backyard\n
+0 Parking\n
+* Side Yard\n
+> More");
 
 simply.on('singleClick', function(e) {
   simply.subtitle('You pressed the ' + e.button + ' button!');
-});
-
-ajax({ url: 'http://arm.local:8080/api/status' }, function(data){
-  simply.body(data);
 });
