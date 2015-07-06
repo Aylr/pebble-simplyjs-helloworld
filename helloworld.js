ad18679daa54cb06ad5362e74cbde6509ce899a9
Aylr <Taylormiller@gmail.com>
2015-07-06 07:15:32 -0600

    4

--- helloworld.js
+++ helloworld.js
@@ -1,9 +1,5 @@
 simply.title('Sprinklers!');
-simply.body("0 Front\n
-0 Backyard\n
-0 Parking\n
-* Side Yard\n
-> More");
+simply.body("0 Front 0 Backyard 0 Parking * Side Yard > More");
 
 simply.on('singleClick', function(e) {
   simply.subtitle('You pressed the ' + e.button + ' button!');
