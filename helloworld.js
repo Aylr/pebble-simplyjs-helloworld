c7bf9860f969d53277eb79f4cf91981086e25637
aylr <taylormiller@gmail.com>
2015-07-06 07:04:57 -0600

    2

--- helloworld.js
+++ helloworld.js
@@ -1,10 +1,9 @@
-simply.title('Hello World!');
+simply.title('Sprinklers!');
 
 simply.on('singleClick', function(e) {
   simply.subtitle('You pressed the ' + e.button + ' button!');
 });
 
 ajax({ url: 'http://arm.local:8080/api/status' }, function(data){
-  var body = data;
-  simply.body(body);
+  simply.body(data);
 });
