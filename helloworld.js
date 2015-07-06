be8df1ebf889ec86a6075ee47b1ada37180453ba
aylr <taylormiller@gmail.com>
2015-07-06 08:09:51 -0600

    5

--- helloworld.js
+++ helloworld.js
@@ -1,10 +1,18 @@
+simply.fullscreen(true);
+
 simply.title('Sprinklers!');
-simply.body("0 Front\n
-0 Backyard\n
-0 Parking\n
-* Side Yard\n
-> More");
+
+simply.body("0 Front 0 Backyard 0 Parking * Side Yard > More");
 
 simply.on('singleClick', function(e) {
-  simply.subtitle('You pressed the ' + e.button + ' button!');
+  simply.body('You pressed the ' + e.button + ' button!');
+
+  if (e.button == "down") {
+  	ajax({ url: 'http://simplyjs.io' }, function(data){
+	  var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
+	  simply.title(headline);
+	});
+  } else if (e.button == "up"){
+  	simply.body("");
+  }
 });
