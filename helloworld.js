a972442fa8215d521003162a08a0605d8410b0d4
aylr <taylormiller@gmail.com>
2015-07-06 20:48:39 -0600

    7
    sprinkler test

--- helloworld.js
+++ helloworld.js
@@ -1,21 +1,36 @@
+var url = "http://arm.local:8080/api/status"
+
+
 simply.fullscreen(true);
 
 simply.title('Sprinklers!');
 
 
-simply.body("0 Front 0 Backyard 0 Parking * Side Yard > More");
+simply.body("Press Up to turn on, down off");
 
 simply.on('singleClick', function(e) {
-  simply.body('You pressed the ' + e.button + ' button!');
+  simply.body('Fetching API request ' + e.button + ' button!');
 
   if (e.button == "down") {
-  	ajax({ url: 'https://api.ipify.org' }, function(data){
-	  var headline = data;
-	  simply.title(headline);
+  	// ajax({ url: url }, function(data){
+	  // var headline = data;
+	  // simply.body(headline);
+
+    simply.body("");
+    simply.title("Station 4 Off");
+    simply.subtitle("");
+
+    ajax({ url: "arm.local:8080/sn?pw=opendoor&sid=4&set_to=0"}, function(data){
+      simply.body(data);
+    });
 	});
   } else if (e.button == "up"){
   	simply.body("");
-    simply.title("");
+    simply.title("Station 4 On");
     simply.subtitle("");
+
+    ajax({ url: "arm.local:8080/sn?pw=opendoor&sid=4&set_to=1"}, function(data){
+      simply.body(data);
+    });
   }
 });
