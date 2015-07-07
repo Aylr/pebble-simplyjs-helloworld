9b9c4680bf161a6717ffb40ac191c2887d6e3ff2
aylr <taylormiller@gmail.com>
2015-07-06 20:50:50 -0600

    7 again

--- helloworld.js
+++ helloworld.js
@@ -9,28 +9,32 @@ simply.title('Sprinklers!');
 simply.body("Press Up to turn on, down off");
 
 simply.on('singleClick', function(e) {
-  simply.body('Fetching API request ' + e.button + ' button!');
+    simply.body('Fetching API request ' + e.button + ' button!');
 
-  if (e.button == "down") {
-  	// ajax({ url: url }, function(data){
-	  // var headline = data;
-	  // simply.body(headline);
+    if (e.button == "down") {
+        // ajax({ url: url }, function(data){
+        // var headline = data;
+        // simply.body(headline);
 
-    simply.body("");
-    simply.title("Station 4 Off");
-    simply.subtitle("");
+        simply.body("");
+        simply.title("Station 4 Off");
+        simply.subtitle("");
 
-    ajax({ url: "arm.local:8080/sn?pw=opendoor&sid=4&set_to=0"}, function(data){
-      simply.body(data);
-    });
-	});
-  } else if (e.button == "up"){
-  	simply.body("");
-    simply.title("Station 4 On");
-    simply.subtitle("");
+        ajax({
+                url: "arm.local:8080/sn?pw=opendoor&sid=4&set_to=0"
+            },
+            function(data) {
+                simply.body(data);
+            });
+    } else if (e.button == "up") {
+        simply.body("");
+        simply.title("Station 4 On");
+        simply.subtitle("");
 
-    ajax({ url: "arm.local:8080/sn?pw=opendoor&sid=4&set_to=1"}, function(data){
-      simply.body(data);
-    });
-  }
+        ajax({
+            url: "arm.local:8080/sn?pw=opendoor&sid=4&set_to=1"
+        }, function(data) {
+            simply.body(data);
+        });
+    }
 });
